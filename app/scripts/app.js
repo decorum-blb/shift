baa2930c8a41b46dad1386306bdc855e17a7c9c8
Adam Edwards <adamcharlesedwards@gmail.com>
2015-01-25 13:13:55 -0600

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name shiftApp
+ * @description
+ * # shiftApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('shiftApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
